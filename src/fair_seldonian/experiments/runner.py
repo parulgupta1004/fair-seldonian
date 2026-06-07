58f594fd0f49f0663e55c15025ebd0491890594f
Parul Gupta <parul100495@gmail.com>
2026-06-07 15:15:35 -0700

    Refactor experiments runner for readability
    - snake_case naming throughout (test_x, n_workers, num_trials, ...)
    - replace overloaded ls_dumb with a clear is_baseline flag and dedupe branches
    - drop redundant numM param (derive from len(ms))

--- src/fair_seldonian/experiments/runner.py
+++ src/fair_seldonian/experiments/runner.py
@@ -19,17 +19,17 @@ DEFAULT_OUTPUT_DIR = "exp/exp_{}/bin/"
 def store_result(
     theta,
     theta1,
-    testX,
-    testY,
-    testT,
-    passedSafetyTest,
+    test_x,
+    test_y,
+    test_t,
+    passed_safety_test,
     worker_id,
-    nWorkers,
+    n_workers,
     m,
     trial,
-    numTrials,
+    num_trials,
     seldonian_type,
-    ls_dumb,
+    is_baseline,
     config=DEFAULT_CONFIG,
 ):
     """
@@ -37,59 +37,51 @@ def store_result(
 
     :param theta: The parameters of the model
     :param theta1: The additional parameter of the model, often the last parameter
-    :param testX: The features of the test dataset
-    :param testY: The labels of the test dataset
-    :param testT: The sensitive attribute column of the test dataset
-    :param passedSafetyTest: Whether the safety test was passed
+    :param test_x: The features of the test dataset
+    :param test_y: The labels of the test dataset
+    :param test_t: The sensitive attribute column of the test dataset
+    :param passed_safety_test: Whether the safety test was passed
     :param worker_id: Id of the worker thread
-    :param nWorkers: Total number of worker threads
+    :param n_workers: Total number of worker threads
     :param trial: Trial number of the experiment on the worker thread
-    :param numTrials: Total number of trials
+    :param num_trials: Total number of trials
     :param seldonian_type: Mode used in the experiment
+    :param is_baseline: Whether this is the unconstrained logistic-regression baseline
     :return: (solution_found, failure_g, upper_bound, fhat) tuple values
     """
-    if ls_dumb:
-        trueLogLoss = float(-f_hat(theta, theta1, testX, testY))
-        upper_bound = float(
-            eval_ghat(theta, theta1, testX, testY, testT, seldonian_type, config)
-        )
-        failures_g1 = 0
-        if upper_bound > 0:
-            failures_g1 = 1
-        w = f"(worker {worker_id}/{nWorkers})"
-        t = f"trial {trial + 1}/{numTrials}"
-        logger.info(
-            f"[{w} {ls_dumb} {t}, m {m}] "
-            f"f_hat: {trueLogLoss:.10f}, upper bound: {upper_bound:.10f}"
-        )
-        return 1, failures_g1, upper_bound, -trueLogLoss
-    elif passedSafetyTest:
-        trueLogLoss = float(-f_hat(theta, theta1, testX, testY))
-        u = float(eval_ghat(theta, theta1, testX, testY, testT, seldonian_type, config))
-        failures_g1 = 0
-        if u > 0:
-            failures_g1 = 1
-        w = f"(worker {worker_id}/{nWorkers})"
-        t = f"trial {trial + 1}/{numTrials}"
-        logger.info(
-            f"[{w} {seldonian_type} {t}, m {m}] Solution: [{theta}, {theta1}] "
-            f"f_hat: {trueLogLoss:.10f}, upper bound: {u:.10f}"
-        )
-        return 1, failures_g1, u, -trueLogLoss
-    else:
-        w = f"(worker {worker_id}/{nWorkers})"
-        t = f"trial {trial + 1}/{numTrials}"
-        logger.info(f"[{w} SBase {t}, m {m}] No solution found")
+    worker = f"(worker {worker_id}/{n_workers})"
+    trial_label = f"trial {trial + 1}/{num_trials}"
+
+    if not is_baseline and not passed_safety_test:
+        logger.info(f"[{worker} SBase {trial_label}, m {m}] No solution found")
         return 0, 0, 0, None
+
+    true_log_loss = float(-f_hat(theta, theta1, test_x, test_y))
+    upper_bound = float(
+        eval_ghat(theta, theta1, test_x, test_y, test_t, seldonian_type, config)
+    )
+    failures_g1 = 1 if upper_bound > 0 else 0
+
+    if is_baseline:
+        logger.info(
+            f"[{worker} LS {trial_label}, m {m}] "
+            f"f_hat: {true_log_loss:.10f}, upper bound: {upper_bound:.10f}"
+        )
+    else:
+        logger.info(
+            f"[{worker} {seldonian_type} {trial_label}, m {m}] "
+            f"Solution: [{theta}, {theta1}] "
+            f"f_hat: {true_log_loss:.10f}, upper bound: {upper_bound:.10f}"
+        )
+    return 1, failures_g1, upper_bound, -true_log_loss
 
 
 def run_experiments(
     worker_id,
-    nWorkers,
+    n_workers,
     ms,
-    numM,
-    numTrials,
-    mTest,
+    num_trials,
+    m_test,
     N,
     seldonian_type,
     config=DEFAULT_CONFIG,
@@ -99,95 +91,95 @@ def run_experiments(
     Main function that runs the experiment.
 
     :param worker_id: Id of the worker thread
-    :param nWorkers: Total number of worker threads
+    :param n_workers: Total number of worker threads
     :param ms: Array containing the fraction values of the amount of data to be used
-    :param numM: Number of fractions of data
-    :param numTrials: Total number of trials
-    :param mTest: The fraction of test samples to be used from the complete dataset
+    :param num_trials: Total number of trials
+    :param m_test: The fraction of test samples to be used from the complete dataset
     :param N: Number of data samples of the synthetic dataset
     :param seldonian_type: Mode used in the experiment
     :return: None
     """
-    s_solutions_found = np.zeros((numTrials, numM))
-    s_failures_g1 = np.zeros((numTrials, numM))
-    s_upper_bound = np.zeros((numTrials, numM))
-    s_fs = np.zeros((numTrials, numM))
+    num_m = len(ms)
+    s_solutions_found = np.zeros((num_trials, num_m))
+    s_failures_g1 = np.zeros((num_trials, num_m))
+    s_upper_bound = np.zeros((num_trials, num_m))
+    s_fs = np.zeros((num_trials, num_m))
 
-    LS_solutions_found = np.zeros((numTrials, numM))
-    LS_failures_g1 = np.zeros((numTrials, numM))
-    LS_upper_bound = np.zeros((numTrials, numM))
-    LS_fs = np.zeros((numTrials, numM))
+    LS_solutions_found = np.zeros((num_trials, num_m))
+    LS_failures_g1 = np.zeros((num_trials, num_m))
+    LS_upper_bound = np.zeros((num_trials, num_m))
+    LS_fs = np.zeros((num_trials, num_m))
 
     experiment_number = worker_id
     output_path = output_dir.format(seldonian_type)
     os.makedirs(output_path, exist_ok=True)
-    outputFile = os.path.join(output_path, f"results{experiment_number}.npz")
-    logger.info(f"Writing output to {outputFile}")
+    output_file = os.path.join(output_path, f"results{experiment_number}.npz")
+    logger.info(f"Writing output to {output_file}")
 
     base_seed = (experiment_number * 99) + 1
     All = get_data(N, 5, 0.4, 0.4, 0.6, base_seed)
     init_sol, init_sol1 = None, None
 
-    for trial in range(numTrials):
-        for mIndex, m in enumerate(ms):
-            base_seed = (experiment_number * numTrials) + 1
+    for trial in range(num_trials):
+        for m_index, m in enumerate(ms):
+            base_seed = (experiment_number * num_trials) + 1
             random_state = base_seed + trial
-            testX, testY, testT, trainX, trainY, trainT = data_split(
-                m, All, random_state, mTest
+            test_x, test_y, test_t, train_x, train_y, train_t = data_split(
+                m, All, random_state, m_test
             )
 
-            theta, theta1 = simple_logistic(trainX, trainY)
+            theta, theta1 = simple_logistic(train_x, train_y)
             (
-                LS_solutions_found[trial, mIndex],
-                LS_failures_g1[trial, mIndex],
-                LS_upper_bound[trial, mIndex],
-                LS_fs[trial, mIndex],
+                LS_solutions_found[trial, m_index],
+                LS_failures_g1[trial, m_index],
+                LS_upper_bound[trial, m_index],
+                LS_fs[trial, m_index],
             ) = store_result(
                 theta,
                 theta1,
-                testX,
-                testY,
-                testT,
+                test_x,
+                test_y,
+                test_t,
                 True,
                 worker_id,
-                nWorkers,
+                n_workers,
                 m,
                 trial,
-                numTrials,
+                num_trials,
                 seldonian_type,
-                "LS",
+                True,
                 config,
             )
 
-            (theta, theta1, passedSafetyTest) = QSA(
-                trainX, trainY, trainT, seldonian_type, init_sol, init_sol1, config
+            theta, theta1, passed_safety_test = QSA(
+                train_x, train_y, train_t, seldonian_type, init_sol, init_sol1, config
             )
             (
-                s_solutions_found[trial, mIndex],
-                s_failures_g1[trial, mIndex],
-                s_upper_bound[trial, mIndex],
-                s_fs[trial, mIndex],
+                s_solutions_found[trial, m_index],
+                s_failures_g1[trial, m_index],
+                s_upper_bound[trial, m_index],
+                s_fs[trial, m_index],
             ) = store_result(
                 theta,
                 theta1,
-                testX,
-                testY,
-                testT,
-                passedSafetyTest,
+                test_x,
+                test_y,
+                test_t,
+                passed_safety_test,
                 worker_id,
-                nWorkers,
+                n_workers,
                 m,
                 trial,
-                numTrials,
+                num_trials,
                 seldonian_type,
-                None,
+                False,
                 config,
             )
-            if s_solutions_found[trial, mIndex] == 1:
+            if s_solutions_found[trial, m_index] == 1:
                 init_sol, init_sol1 = theta, theta1
 
     np.savez(
-        outputFile,
+        output_file,
         ms=ms,
         s_solutions_found=s_solutions_found,
         s_fs=s_fs,
@@ -198,7 +190,7 @@ def run_experiments(
         LS_failures_g1=LS_failures_g1,
         LS_upper_bound=LS_upper_bound,
     )
-    logger.info(f"Saved the file {outputFile}")
+    logger.info(f"Saved the file {output_file}")
 
 
 if __name__ == "__main__":
@@ -212,24 +204,23 @@ if __name__ == "__main__":
     run_experiments_remote = ray.remote(run_experiments)
 
     logger.info("Assuming the default: 50")
-    nWorkers = 2
-    logger.info(f"Running experiments on {nWorkers} threads")
+    n_workers = 2
+    logger.info(f"Running experiments on {n_workers} threads")
     N = 10000
     ms = np.logspace(-2, 0, num=3)
     logger.info(f"N {N}, frac array: {ms}")
     logger.info(f"Running for: {sys.argv[1]}")
-    numM = len(ms)
-    numTrials = 2
-    mTest = 0.2
-    logger.info(f"Number of trials: {numTrials}")
+    num_trials = 2
+    m_test = 0.2
+    logger.info(f"Number of trials: {num_trials}")
 
     tic = timeit.default_timer()
     _ = ray.get(
         [
             run_experiments_remote.remote(
-                worker_id, nWorkers, ms, numM, numTrials, mTest, N, sys.argv[1]
+                worker_id, n_workers, ms, num_trials, m_test, N, sys.argv[1]
             )
-            for worker_id in range(1, nWorkers + 1)
+            for worker_id in range(1, n_workers + 1)
         ]
     )
     toc = timeit.default_timer()
